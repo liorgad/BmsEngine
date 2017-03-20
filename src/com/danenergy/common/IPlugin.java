78a1cf9273fdb0395b6fc37e6c69bc31300b0fc7
liorgad <liorgad@gmail.com>
2017-03-20 08:53:43 +0200

    added plugin interface

---
+++ src/com/danenergy/common/IPlugin.java
@@ -0,0 +1,11 @@
+package com.danenergy.common;
+
+/**
+ * Created by Lior Gad on 3/20/2017.
+ */
+public interface IPlugin {
+    public void Start();
+    public void Stop();
+    public void Dispose();
+
+}
