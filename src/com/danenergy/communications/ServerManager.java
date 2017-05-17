ea9ce4de92a756132d91831a48414d1259b00cae
liorgad@gmail.com <lior@danenergy.com>
2017-05-17 12:17:11 +0300

    working ui and engine

--- src/com/danenergy/communications/ServerManager.java
+++ src/com/danenergy/communications/ServerManager.java
@@ -4,11 +4,12 @@ import com.danenergy.common.EventBusMessages.IncommingBmsData;
 import com.danenergy.common.EventBusMessages.StartServerManagerMessage;
 import com.danenergy.common.ICommPort;
 import com.danenergy.common.IPlugin;
-import com.danenergy.configuration.Data;
-import com.danenergy.parser.GenericParser;
-import com.danenergy.protocol.Command;
-import com.danenergy.protocol.FrameFormat;
-import com.danenergy.protocol.Version;
+import com.danenergy.common.Configuration;
+import com.danenergy.common.Data;
+import com.danenergy.common.parser.GenericParser;
+import com.danenergy.common.protocol.Command;
+import com.danenergy.common.protocol.FrameFormat;
+import com.danenergy.common.protocol.Version;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
@@ -20,9 +21,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.function.Consumer;
 
-/**
- * Created by Lior Gad on 3/20/2017.
- */
 public class ServerManager implements IPlugin {
 
     //logging
@@ -32,18 +30,22 @@ public class ServerManager implements IPlugin {
     ICommPort commPort;
     Timer timer;
     Data sharedData;
+    Configuration config;
 
     @Inject
-    public ServerManager(EventBus eventBus,ICommPort commPort,Data sharedData) {
+    public ServerManager(EventBus eventBus,ICommPort commPort,Data sharedData,Configuration config) {
         this.eventBus = eventBus;
         this.commPort = commPort;
         this.sharedData = sharedData;
+        this.config = config;
     }
 
     @Override
     public void Start() {
         eventBus.register(this);
         logger.info("ServerManager Started");
+        commPort.initializePort(this.config.getPortName());
+        commPort.open();
 
         timer = new Timer(true);
     }
@@ -54,6 +56,11 @@ public class ServerManager implements IPlugin {
 
     @Override
     public void Stop() {
+
+         eventBus.unregister(this);
+         commPort.close();
+         timer.cancel();
+
         logger.info("ServerManager Stopped");
     }
 
@@ -72,67 +79,76 @@ public class ServerManager implements IPlugin {
     @Subscribe
     public void handleStartServerManager(StartServerManagerMessage msg)
     {
-        completeTask();
+        //completeTask();
+        int threadWaitForEachBattery = config.getWaitTimePeriodBetweenCommandSendMilliSec();
 
-//        timer.scheduleAtFixedRate(new TimerTask() {
-//            @Override
-//            public void run() {
-//                logger.info("Timer task started at:"+new Date());
-//                completeTask();
-//                logger.info("Timer task finished at:"+new Date());
-//            }
-//            private void completeTask() {
-//                try {
-//
-//                    //assuming it takes 2 secs to complete the task
-//                    //Thread.sleep(2000);
-//                    sharedData.getDefinedAddresses().stream().forEach(new Consumer<String>() {
-//                        @Override
-//                        public void accept(String s) {
-//
-//                            FrameFormat ff = new FrameFormat();
-//                            ff.Address = Short.parseShort(s);
-//                            ff.Cmd = (short)Command.RealTimeData.getValue();
-//                            ff.Version = (short)Version.Version82.getValue();
-//                            ff.Length = ff.CalculateLength();
-//
-//                            String cmd = GenericParser.Build(ff,FrameFormat.class);
-//
-//                            String cmdSub = cmd.substring(1,cmd.length()-3);
-//
-//                            String crc = FrameFormat.CalculateCRC(cmdSub);
-//
-//                            String finalCmd = FrameFormat.SOI + cmdSub + crc + FrameFormat.EOI;
-//
-//                            if(!commPort.isOpen())
-//                            {
-//                                return;
-//                            }
-//
-//                            logger.info("Sending " + finalCmd);
-//
-//                            String result = commPort.sendReceive(finalCmd);
-//
-//                            logger.info("Received " + result);
-//
-//                            if(StringUtils.isEmpty(result))
-//                            {
-//                                return;
-//                            }
-//
-//                            IncommingBmsData inData = new IncommingBmsData();
-//                            inData.data = result;
-//                            publishIncomingData(inData);
-//                        }
-//                    });
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//        }, 0, 10 * 1000);
-//        logger.info("TimerTask started");
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                logger.info("Timer task started at:"+new Date());
+                completeTask();
+                logger.info("Timer task finished at:"+new Date());
+            }
+            private void completeTask() {
+                try {
+
+                    //assuming it takes 2 secs to complete the task
+                    //Thread.sleep(2000);
+                    sharedData.getDefinedAddresses().stream().forEach(new Consumer<String>() {
+                        @Override
+                        public void accept(String s) {
+
+                            FrameFormat ff = new FrameFormat();
+                            ff.Address = Short.parseShort(s);
+                            ff.Cmd = (short)Command.RealTimeData.getValue();
+                            ff.Version = (short)Version.Version82.getValue();
+                            ff.Length = ff.CalculateLength();
+
+                            String cmd = GenericParser.Build(ff,FrameFormat.class);
+
+                            String cmdSub = cmd.substring(1,cmd.length()-3);
+
+                            String crc = FrameFormat.CalculateCRC(cmdSub);
+
+                            String finalCmd = FrameFormat.SOI + cmdSub + crc + FrameFormat.EOI;
+
+                            if(!commPort.isOpen())
+                            {
+                                logger.warn("CommPort is closed");
+                                return;
+                            }
+
+                            logger.info("Sending " + finalCmd);
+
+                            String result = commPort.sendReceive(finalCmd);
+
+                            logger.info("Received " + result);
+
+                            if(StringUtils.isEmpty(result))
+                            {
+                                return;
+                            }
+
+                            IncommingBmsData inData = new IncommingBmsData();
+                            inData.data = result;
+                            publishIncomingData(inData);
+                            try {
+                                Thread.sleep((long) threadWaitForEachBattery);
+                            }
+                            catch (InterruptedException e)
+                            {
+                                logger.debug("Sampling thread interrupted",e);
+                            }
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }, 0, config.getSamplingTimerIntervalMilisec().intValue());
+        logger.info("TimerTask started");
 
 
         //cancel after sometime
@@ -150,44 +166,53 @@ public class ServerManager implements IPlugin {
 //        }
     }
 
-    private void completeTask() {
-        try {
-
-            FrameFormat ff = new FrameFormat();
-            ff.Address = Short.parseShort("3");
-            ff.Cmd = (short) Command.RealTimeData.getValue();
-            ff.Version = (short) Version.Version82.getValue();
-            ff.Length = ff.CalculateLength();
-
-            String cmd = GenericParser.Build(ff, FrameFormat.class);
-
-            String cmdSub = cmd.substring(1, cmd.length() - 3);
-
-            String crc = FrameFormat.CalculateCRC(cmdSub);
-
-            String finalCmd = FrameFormat.SOI + cmdSub + crc + FrameFormat.EOI;
-
-            if (!commPort.isOpen()) {
-                return;
-            }
-
-            logger.info("Sending " + finalCmd);
-
-            String result = commPort.sendReceive(finalCmd);
-
-            logger.info("Received " + result);
-
-            if (StringUtils.isEmpty(result)) {
-                return;
-            }
-
-            IncommingBmsData inData = new IncommingBmsData();
-            inData.data = result;
-            publishIncomingData(inData);
-        }
-        catch (Exception e)
-        {
-
-        }
-    }
+//    private void completeTask() {
+//        try {
+//            //assuming it takes 2 secs to complete the task
+//            //Thread.sleep(2000);
+//
+//            sharedData.getDefinedAddresses().stream().forEach(new Consumer<String>() {
+//                @Override
+//                public void accept(String s) {
+//
+//                    FrameFormat ff = new FrameFormat();
+//                    ff.Address = Short.parseShort(s);
+//                    ff.Cmd = (short)Command.RealTimeData.getValue();
+//                    ff.Version = (short)Version.Version82.getValue();
+//                    ff.Length = ff.CalculateLength();
+//
+//                    String cmd = GenericParser.Build(ff,FrameFormat.class);
+//
+//                    String cmdSub = cmd.substring(1,cmd.length()-3);
+//
+//                    String crc = FrameFormat.CalculateCRC(cmdSub);
+//
+//                    String finalCmd = FrameFormat.SOI + cmdSub + crc + FrameFormat.EOI;
+//
+//                    if(!commPort.isOpen())
+//                    {
+//                        return;
+//                    }
+//
+//                    logger.info("Sending " + finalCmd);
+//
+//                    String result = commPort.sendReceive(finalCmd);
+//
+//                    logger.info("Received " + result);
+//
+//                    if(StringUtils.isEmpty(result))
+//                    {
+//                        return;
+//                    }
+//
+//                    IncommingBmsData inData = new IncommingBmsData();
+//                    inData.data = result;
+//                    publishIncomingData(inData);
+//                }
+//            });
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
